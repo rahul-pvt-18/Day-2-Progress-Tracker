9b25927c8d0d950d56ca28bece0eee88c6adffb0
rahul-pvt-18 <100333864+rahul-pvt-18@users.noreply.github.com>
2023-06-18 17:44:18 +0530

    first commit

---
+++ Day 2 -  Progress Steps/script.js
@@ -0,0 +1,49 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click',() => {
+    currentActive++
+    if(currentActive > circles.length){
+        currentActive  = circles.length
+    }
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    if(currentActive < 1){
+        currentActive = 1
+    }
+    update()
+})
+
+function update(){
+    circles.forEach((circle,i) => {
+        if(i < currentActive){
+            circle.classList.add('active')
+        }
+        else{
+            circle.classList.remove('active')
+        } 
+    })
+
+    const actives = document.querySelectorAll('.active')
+    const percentageWidth = ((actives.length-1)/(circles.length-1))*100+'%'
+    progress.style.width = percentageWidth
+
+    // Enabling and disabling Previous and Next Buttons 
+    if(currentActive === 1 ){
+        prev.disabled = true
+    }
+    else if(currentActive == circles.length){
+        next.disabled = true
+    }
+    else{
+        prev.disabled = false
+        next.disbled = false
+    }
+}
